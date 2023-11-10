956787078719efe60b91cdbf4a01be14467a429c
nzc5325 <nzc5325@psu.edu>
2023-11-10 00:04:32 -0500

    SDL and question 8

---
+++ Homework_2.h
@@ -0,0 +1,12 @@
+﻿// Homework 2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <random>
+#include <vector>
+
+// TODO: Reference additional headers your program requires here.
