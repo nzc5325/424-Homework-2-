34d19146ac2505e5c9d1c0f8ef3ce91a3a3faf33
nzc5325 <nzc5325@psu.edu>
2023-11-10 11:53:18 -0500

    Question 8 not right but working

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/nicol/source/repos/Homework 2
+# Install script for directory: C:/Users/nicol/source/repos/Homework2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/nicol/source/repos/Homework 2/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/nicol/source/repos/Homework2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
